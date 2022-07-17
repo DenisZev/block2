59ec0a307da21e0d0d8aeeb2a67f79301bc7febe
zevae <deniszev68@gmail.com>
2022-07-17 23:33:16 +0700

    update

--- script.js
+++ script.js
@@ -40,11 +40,46 @@ $(function(){
 
 const expandImg = document.querySelector('#expandedImg');
 let imgg = document.querySelectorAll('.product__gallery');
-console.log(imgg);
+const urlPic = document.querySelectorAll('.large')
 
 $('.product__gallery').on('mouseenter', 'img', function (){
 	const imageUrl = $(this).attr('src');
+	const backUrl = $(this).css('background', imageUrl)
 	expandImg.src = imageUrl;
+	$('.large').css({'background': 'url(' + imageUrl + ')'});
+	urlPic.background = imageUrl;
+	console.log(backUrl)
+	console.log(imageUrl)
 })
 
+// zoom
 
+
+$(document).ready(function(){
+	const zoomMargin = 50;
+
+	function startZoom(e) {
+		$('.large').css('left', $(this).width() + zoomMargin).show();    // Контейнер у большим изображением становится видимым.
+	}
+
+	function moveZoom(e) {
+		const offset = $(this).offset(),       // В переменной offset хранятся координаты блока с миниатюрой
+			x = (e.pageX - offset.left),      // В переменной хранится координата X курсора мыши относительно блока с миниатюрой
+			y = (e.pageY - offset.top);        // Координата Y курсора мыши.
+			w = $(this).width(),               // Ширина миниатюры
+			h = $(this).height(),              // Высота миниатюры
+
+			// Позиционирование фона большого изображения относительно того, куда указывает курсор на миниатюре.
+			$('.large').css({'background-position': (x / w * 90) + '% ' + (y / h * 90) + '%'});
+	}
+
+	function endZoom(e) {
+		$('.large').hide();        // Контейнер с большим изображением скрывается
+	}
+
+	$('#expandedImg').on({
+		'mouseenter': startZoom,  // При наведении мыши на миниатюру срабатывает функция startZoom
+		'mousemove': moveZoom,    // Когда курсор мыши перемещается внутри миниатюры срабатывает функция moveZoom
+		'mouseleave': endZoom     // Когда мышь уходит из области миниатюры срабатывает функция endZoom
+	});
+})
